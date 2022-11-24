f7e3037bb1b2464ccaff285df12f4f7d7d86982f
Yozora0 <pablo.mourato@etudiant.univ-reims.fr>
2022-11-25 00:45:42 +0100

    entité manifs, dossier img et affichage de toutes les manifs (hormis les img qui n'apparaissent pas)

--- src/Controller/ManifsController.php
+++ src/Controller/ManifsController.php
@@ -2,17 +2,26 @@
 
 namespace App\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Manifs;
+use App\Repository\ManifsRepository;
+use Doctrine\ORM\EntityManagerInterface;
 
 class ManifsController extends AbstractController
 {
     #[Route('/manifs', name: 'app_manifs')]
-    public function index(): Response
+    public function listeManifs(ManifsRepository $ManifsRepository)
     {
+        $manifs = $ManifsRepository->findAll();
+
+
         return $this->render('manifs/index.html.twig', [
-            'controller_name' => 'ManifsController',
+            'manifs' => $manifs,
         ]);
     }
+
+
 }
---
+++ src/Entity/Manifs.php
@@ -0,0 +1,171 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\ManifsRepository;
+use Doctrine\DBAL\Types\Types;
+use Doctrine\ORM\Mapping as ORM;
+
+#[ORM\Entity(repositoryClass: ManifsRepository::class)]
+class Manifs
+{
+    #[ORM\Id]
+    #[ORM\GeneratedValue]
+    #[ORM\Column]
+    private ?int $id = null;
+
+    #[ORM\Column(length: 100)]
+    private ?string $titre = null;
+
+    #[ORM\Column(type: Types::TEXT)]
+    private ?string $description = null;
+
+    #[ORM\Column(length: 25)]
+    private ?string $genre = null;
+
+    #[ORM\Column(type: Types::TEXT, nullable: true)]
+    private ?string $casting = null;
+
+    #[ORM\Column(length: 255)]
+    private ?string $affiche = null;
+
+    #[ORM\Column(length: 10)]
+    private ?string $tarif = null;
+
+    #[ORM\Column(length: 150)]
+    private ?string $lieu = null;
+
+    #[ORM\Column(type: Types::DATE_MUTABLE)]
+    private ?\DateTimeInterface $date = null;
+
+    #[ORM\Column(type: Types::TIME_MUTABLE)]
+    private ?\DateTimeInterface $horaire = null;
+
+    #[ORM\Column]
+    private ?int $capacite = null;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getTitre(): ?string
+    {
+        return $this->titre;
+    }
+
+    public function setTitre(string $titre): self
+    {
+        $this->titre = $titre;
+
+        return $this;
+    }
+
+    public function getDescription(): ?string
+    {
+        return $this->description;
+    }
+
+    public function setDescription(string $description): self
+    {
+        $this->description = $description;
+
+        return $this;
+    }
+
+    public function getGenre(): ?string
+    {
+        return $this->genre;
+    }
+
+    public function setGenre(string $genre): self
+    {
+        $this->genre = $genre;
+
+        return $this;
+    }
+
+    public function getCasting(): ?string
+    {
+        return $this->casting;
+    }
+
+    public function setCasting(?string $casting): self
+    {
+        $this->casting = $casting;
+
+        return $this;
+    }
+
+    public function getAffiche(): ?string
+    {
+        return $this->affiche;
+    }
+
+    public function setAffiche(string $affiche): self
+    {
+        $this->affiche = $affiche;
+
+        return $this;
+    }
+
+    public function getTarif(): ?string
+    {
+        return $this->tarif;
+    }
+
+    public function setTarif(string $tarif): self
+    {
+        $this->tarif = $tarif;
+
+        return $this;
+    }
+
+    public function getLieu(): ?string
+    {
+        return $this->lieu;
+    }
+
+    public function setLieu(string $lieu): self
+    {
+        $this->lieu = $lieu;
+
+        return $this;
+    }
+
+    public function getDate(): ?\DateTimeInterface
+    {
+        return $this->date;
+    }
+
+    public function setDate(\DateTimeInterface $date): self
+    {
+        $this->date = $date;
+
+        return $this;
+    }
+
+    public function getHoraire(): ?\DateTimeInterface
+    {
+        return $this->horaire;
+    }
+
+    public function setHoraire(\DateTimeInterface $horaire): self
+    {
+        $this->horaire = $horaire;
+
+        return $this;
+    }
+
+    public function getCapacite(): ?int
+    {
+        return $this->capacite;
+    }
+
+    public function setCapacite(int $capacite): self
+    {
+        $this->capacite = $capacite;
+
+        return $this;
+    }
+}
